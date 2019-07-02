6faa1b098800ade2e30bab676a0942f66b750ea0
Ricardo Cabral <rramoscabral@gmail.com>
2019-07-02 01:34:07 +0100

    Update

---
+++ UWP/Mod03/Demos/UWP-BindingDemo/UWP-DataBindingDemo/Data/Pearson.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_DataBindingDemo.Data
+{
+    public class Pearson
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Pearson()
+        {
+            this.ID = 1;
+            this.Name = "Anya Silva";
+            this.Email = "asilva@anya-silva.pt";
+        }
+        public string Details
+        {
+            get
+            {
+                return $"{this.ID} - {this.Name}, correio electrónico: "
+                    + this.Email;
+            }
+        }
+    }
+
+    public class PearsonViewModel
+    {
+        private Pearson defaultPearson = new Pearson();
+        public Pearson DefaultPearson => this.defaultPearson;
+    }
+
+
+    public class PearsonViewModelCollection
+    {
+
+        private ObservableCollection<Pearson> pearsons = new ObservableCollection<Pearson>();
+
+        public ObservableCollection<Pearson> Pearsons { get { return this.pearsons; } }
+
+        public PearsonViewModelCollection()
+        {
+            this.Pearsons.Add(new Pearson()
+            {
+                ID = 1,
+                Name = "Zen Carmo",
+                Email = "zcarmo@xpto.pt"
+            });
+            this.Pearsons.Add(new Pearson()
+            {
+                ID = 2,
+                Name = "Filipa Marques",
+                Email = "fm@fm.pt"
+            });
+        }
+    }
+}
+
---
+++ UWP/Mod03/Demos/UWP-BindingDemo/UWP-DataBindingDemo/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWP_DataBindingDemo.Data;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_DataBindingDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Propriedade ViewModel que vai ser utilizado no Binding:
+        //    <TextBlock Text="{x:Bind ViewModel.DefaultPearson.Details}"
+        public PearsonViewModel ViewModel { get; set; }
+
+
+        // Propriedade ViewModelCollection que vai ser utilizado no Binding:
+        //    <ListView ItemsSource="{x:Bind ViewModelCollection.Pearsons}
+        public PearsonViewModelCollection ViewModelCollection { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new PearsonViewModel();
+            this.ViewModelCollection = new PearsonViewModelCollection();
+        }
+       
+
+    }
+}
