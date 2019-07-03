d0e1ba595c5917f1d156591806faf8c26d3fe77d
Ricardo Cabral <rramoscabral@gmail.com>
2019-07-04 00:21:55 +0100

    Update

---
+++ UWP/Mod06/Demos/RoamingData/README.md
@@ -0,0 +1,2 @@
+# RoamingDataUWP
+Sample project of using Roamind Data on UWP. For single device test, change the background, close the app, and then open it again.
---
+++ UWP/Mod06/Demos/RoamingData/RoamingDataUWP/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+
+namespace RoamingDataUWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public List<string> Backgrounds { get; set; }
+        ApplicationDataContainer roamingSettings;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Backgrounds = new List<string>{"ms-appx:///Assets/1.jpg", "ms-appx:///Assets/2.jpg", "ms-appx:///Assets/3.jpg", "ms-appx:///Assets/4.jpg"};
+            roamingSettings = ApplicationData.Current.RoamingSettings;
+
+            if (roamingSettings.Values.ContainsKey("backgroundImage"))
+            {
+                BackgroundImage.UriSource = new Uri(roamingSettings.Values["backgroundImage"].ToString());
+            }
+        }
+
+        private void GridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var backgroundImage = e.AddedItems[0] as string;
+            BackgroundImage.UriSource = new Uri(backgroundImage);
+            roamingSettings.Values["backgroundImage"] = backgroundImage;
+        }
+    }
+}
